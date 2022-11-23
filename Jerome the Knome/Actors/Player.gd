02b95bf9bcc2785e208210d8c428af78cd1386c1
Ben <bmricker@outlook.com>
2022-11-22 22:31:53 -0500

    Smoothed the movement and added an idle animation

--- Jerome the Knome/Actors/Player.gd
+++ Jerome the Knome/Actors/Player.gd
@@ -1,14 +1,19 @@
 extends Actor
 
+const MAX_SPEED = 100
+const ACCELLERATION = 300
+const FRICTION = 300
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		_velocity = input_vector
+		_velocity = _velocity.move_toward(input_vector * MAX_SPEED, ACCELLERATION * delta)
 	else:
-		_velocity = Vector2.ZERO
-	move_and_collide(_velocity)
+		_velocity = _velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	move_and_collide(_velocity * delta)
 
